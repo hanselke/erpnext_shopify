deb08bd71dc43a826cb18f8935e6ee8b47e8894c
Leonard Shi <leonard@Leonards-MacBook-Air.local>
2015-11-13 21:42:31 +0800

    one more test

--- erpnext_shopify/erpnext_shopify/doctype/shopify_settings/shopify_settings.py
+++ erpnext_shopify/erpnext_shopify/doctype/shopify_settings/shopify_settings.py
@@ -379,11 +379,11 @@ def validate_customer_and_product(order):
     if not frappe.db.get_value("Customer", {"shopify_id": order.get("customer").get("id")}, "name"):
         create_customer(order.get("customer"))
     
-    warehouse = frappe.get_doc("Shopify Settings", "Shopify Settings").warehouse
-    for item in order.get("line_items"):
-        if not frappe.db.get_value("Item", {"shopify_id": item.get("product_id")}, "name"):
-            item = get_request("/admin/products/{}.json".format(item.get("product_id")))["product"]
-            make_item(warehouse, item)
+    # warehouse = frappe.get_doc("Shopify Settings", "Shopify Settings").warehouse
+    # for item in order.get("line_items"):
+    #     if not frappe.db.get_value("Item", {"shopify_id": item.get("product_id")}, "name"):
+    #         item = get_request("/admin/products/{}.json".format(item.get("product_id")))["product"]
+    #         make_item(warehouse, item)
 
 def get_shopify_id(item):pass
         
