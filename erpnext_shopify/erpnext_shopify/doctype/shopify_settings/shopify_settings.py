c24154e9cf2b12aaa66f787fb0a14c87a5836b95
Leonard Shi <leonard.shi.sg@gmail.com>
2016-01-04 21:35:25 +0800

    revert test.

--- erpnext_shopify/erpnext_shopify/doctype/shopify_settings/shopify_settings.py
+++ erpnext_shopify/erpnext_shopify/doctype/shopify_settings/shopify_settings.py
@@ -41,8 +41,8 @@ def sync_shopify():
     if shopify_settings.enable_shopify:
         try :
             # sync_customers()
-            sync_products(shopify_settings.price_list, shopify_settings.warehouse)
-            # sync_orders()
+            # sync_products(shopify_settings.price_list, shopify_settings.warehouse)
+            sync_orders()
             
         except ShopifyError:
             raise ValueError(ShopifyError)
@@ -54,7 +54,7 @@ def sync_products(price_list, warehouse):
 def sync_shopify_items(warehouse):
     shopify_items = get_shopify_items()
     # Workaround for this too long operation, 261
-    for item in shopify_items[250:300]:
+    for item in shopify_items:
         make_item(warehouse, item)
 
 def make_item(warehouse, item):
@@ -393,7 +393,7 @@ def sync_shopify_orders():
 
     orders = sorted(orders, key=lambda x: datetime.datetime.strptime(x["processed_at"][:-6], "%Y-%m-%dT%H:%M:%S"))
 
-    for order in orders:
+    for order in orders[0:10]:
         if not order.get("customer"):
             order["customer"] = {}
             order["customer"]["total_spent"] = order["subtotal_price"]
