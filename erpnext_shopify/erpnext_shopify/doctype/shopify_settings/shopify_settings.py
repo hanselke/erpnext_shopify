cf437666d88a3586bfb364d691cf1fa80a6e2789
Leonard Shi <leonard.shi.sg@gmail.com>
2016-01-05 16:13:36 +0800

    one more testing.

--- erpnext_shopify/erpnext_shopify/doctype/shopify_settings/shopify_settings.py
+++ erpnext_shopify/erpnext_shopify/doctype/shopify_settings/shopify_settings.py
@@ -295,7 +295,7 @@ def sync_shopify_orders():
     orders = sorted(orders, key=lambda x: datetime.datetime.strptime(x["processed_at"][:-6], "%Y-%m-%dT%H:%M:%S"))
 
     # 502
-    for order in orders[255:270]:
+    for order in orders[270:285]:
         if not order.get("customer"):
             order["customer"] = {}
             order["customer"]["total_spent"] = order["subtotal_price"]
