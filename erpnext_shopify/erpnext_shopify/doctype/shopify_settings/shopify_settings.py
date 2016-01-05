144d3cbba10eaf346647d7e6866a0f2160cdaf10
Leonard Shi <leonard.shi.sg@gmail.com>
2016-01-05 16:51:27 +0800

    revert testing.

--- erpnext_shopify/erpnext_shopify/doctype/shopify_settings/shopify_settings.py
+++ erpnext_shopify/erpnext_shopify/doctype/shopify_settings/shopify_settings.py
@@ -40,8 +40,8 @@ def sync_shopify():
         
     if shopify_settings.enable_shopify:
         try :
-            # sync_customers()
-            # sync_products(shopify_settings.price_list, shopify_settings.warehouse)
+            sync_customers()
+            sync_products(shopify_settings.price_list, shopify_settings.warehouse)
             sync_orders()
             
         except ShopifyError:
@@ -290,12 +290,12 @@ def sync_orders():
     sync_shopify_orders()
 
 def sync_shopify_orders():
-    orders = filter(lambda x: datetime.datetime.strptime(x["processed_at"][:-6], "%Y-%m-%dT%H:%M:%S") > datetime.datetime.strptime('2015-11-17T00:00:00' ,'%Y-%m-%dT%H:%M:%S'), get_shopify_orders())
+    orders = filter(lambda x: datetime.datetime.strptime(x["processed_at"][:-6], "%Y-%m-%dT%H:%M:%S") > datetime.datetime.strptime('2016-01-01T00:00:00' ,'%Y-%m-%dT%H:%M:%S'), get_shopify_orders())
 
     orders = sorted(orders, key=lambda x: datetime.datetime.strptime(x["processed_at"][:-6], "%Y-%m-%dT%H:%M:%S"))
 
     # 502
-    for order in orders[502:510]:
+    for order in orders:
         if not order.get("customer"):
             order["customer"] = {}
             order["customer"]["total_spent"] = order["subtotal_price"]
