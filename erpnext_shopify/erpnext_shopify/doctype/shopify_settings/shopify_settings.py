83aee077bf3e426f8748893dfc52ef2eb061330e
Leonard Shi <leonard.shi.sg@gmail.com>
2016-05-31 20:23:23 +0800

    test.

--- erpnext_shopify/erpnext_shopify/doctype/shopify_settings/shopify_settings.py
+++ erpnext_shopify/erpnext_shopify/doctype/shopify_settings/shopify_settings.py
@@ -44,7 +44,7 @@ def sync_shopify():
     if shopify_settings.enable_shopify:
         try :
             sync_customers()
-            sync_products(shopify_settings.price_list, shopify_settings.warehouse)
+            # sync_products(shopify_settings.price_list, shopify_settings.warehouse)
             # sync_orders()
             
         except ShopifyError:
